c6ba18e9548059d2699d4b471ee657a486b8e56f
azzam jafri <azzamjafri98@gmail.com>
2019-03-15 01:34:15 +0530

    Register

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
+from .models import post
 
-post = [
-	{
-		'author': 'Azzam jafri',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'August 27 , 2k19'
-	},
-	{
-		'author': 'Randon Person',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'March 27 , 2k19'
-	}
-]
+
 
 # Create your views here.
 
 def home(request):
 	context = {
-		'posts': post
+		'posts': post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
