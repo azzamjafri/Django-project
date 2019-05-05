a4832566babedc2265892dd86d58197d5f41a014
azzam jafri <azzamjafri98@gmail.com>
2019-05-05 23:54:12 +0530

    deploying

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't35o_u&-f-kayhvwdw(fk3g1(ninc)jto&ju7wm8jcyep^6@p)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
